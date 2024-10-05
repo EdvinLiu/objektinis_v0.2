1936e94e10ae9d2f16cbdc10b85f9d5681efdebf
EdvinLiu <edwinlub132@gmail.com>
2024-10-05 19:52:48 +0300

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -9,5 +9,6 @@ double galutinis(const std::vector<int>& nd, int egzaminas);
 double mediana(const std::vector<int>& nd, int egzaminas);
 int generuoti_atsitiktini(int min, int max);
 void nuskaityti_faila(const std::string& failo_pavadinimas, std::vector<Studentas>& studentai);
+void generuoti_studentus_failui(const string& failo_pavadinimas, int studentu_skaicius);
 
 #endif // FUNCTIONS_H_INCLUDED
