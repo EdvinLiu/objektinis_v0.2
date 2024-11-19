ba7b3586a6b557453b0090dc75aee6ba0e0f5145
EdvinLiu <edwinlub132@gmail.com>
2024-11-19 11:03:34 +0200

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -23,43 +23,6 @@ int generuoti_atsitiktini(int min, int max) {
     srand(time(nullptr));
     return min + rand() % (max - min + 1);
 }
-
-void nuskaityti_faila(const string& failo_pavadinimas, vector<Studentas>& studentai) {
-    ifstream failas(failo_pavadinimas);
-    if (!failas.is_open()) {
-        cout << "Nepavyko atidaryti failo: " << failo_pavadinimas << endl;
-        return;
-    }
-
-    string antraste;
-    getline(failas, antraste);
-
-    string line;
-    while (getline(failas, line)) {
-        if (!line.empty()) {
-            istringstream line_stream(line);  // Sukuriame srautą eilutei
-            Studentas studentas;
-            line_stream >> studentas.vardas >> studentas.pavarde;  // Nuskaitome vardą ir pavardę
-
-            int pazym;
-            while (line_stream >> pazym) {
-                studentas.namuDarbai.push_back(pazym);  // Dedame pažymius
-            }
-
-            // Egzamino pažymys yra paskutinis, todėl priskiriame jį atskirai
-            if (!studentas.namuDarbai.empty()) {
-                studentas.egzaminas = studentas.namuDarbai.back();  // Paskutinis pažymys yra egzaminas
-                studentas.namuDarbai.pop_back();  // Pašaliname egzaminą iš namų darbų sąrašo
-            }
-
-            studentai.push_back(studentas);  // Pridedame studentą į sąrašą
-        }
-    }
-
-    failas.close();  // Uždaryti failą
-}
-
-
 // Funkcija, kuri sugeneruoja atsitiktinius namų darbų pažymius ir egzaminą
 vector<int> generuoti_atsitiktinius_pazymius() {
     random_device rd;
@@ -100,32 +63,14 @@ void generuoti_studentus_failui(const string& failo_pavadinimas, int studentu_sk
 
     failas.close();
 }
-void suskirstyti_studentus(const vector<Studentas>& studentai, char pasirinkite) {
-    vector<Studentas> vargsiukai; // Studentai su galutiniu balu < 5
-    vector<Studentas> kietiakiai; // Studentai su galutiniu balu >= 5
-
-    for (const auto& studentas : studentai) {
-        if (studentas.galutinis < 5.0) {
-            // Pridedame studentą į vargšiukų vektorių
-            vargsiukai.push_back(studentas);
-        }
-        else {
-            // Pridedame studentą į kietiakio vektorių
-            kietiakiai.push_back(studentas);
-        }
-    }
-    failo_spausdinimas(vargsiukai, "vargsiukai.txt", pasirinkite);
-    failo_spausdinimas(kietiakiai, "kietiakiai.txt", pasirinkite);
-
-}
 void failo_spausdinimas(const vector<Studentas>& studentai, string failas, char pasirinkite)
 {
     ofstream fr(failas);
     fr << setw(15) << left << "Vardas" << setw(15) << "Pavarde" << setw(15)
         << (pasirinkite == 'v' ? "galutinis(Vid.)" : "Galutinis(Med.)") << endl;
     for (const auto& studentas : studentai) {
-        fr << setw(15) << left << studentas.vardas << setw(15) << studentas.pavarde
-            << setw(10) << fixed << setprecision(2) << studentas.galutinis << endl;
+        fr << setw(15) << left << studentas.vardas() << setw(15) << studentas.pavarde()
+            << setw(10) << fixed << setprecision(2) << studentas.galutinis() << endl;
     }
     fr.close();
 }
